4e4468c90474bff669555705d4667ed3b157f3e9
Julia-Glazovskaya <uglazovskaya@gmail.com>
2019-09-30 16:10:06 +0300

    firstcommit

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=zverev',
+    'dsn' => 'mysql:host=localhost;dbname=julia',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
